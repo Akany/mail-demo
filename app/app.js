fe15692b211c79e52bef1aa21846c3dd8c60ca64
sif <sif>
2017-01-30 00:06:01 +0200

    added header

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 import ng from 'angular';
 import template from './app.html';
+import headerComponent from './components/header/header.component';
 
 export default ng.module('mail', ['ngMaterial'])
     .component('app', {
         template: template
-    });
+    })
+    .component('header', headerComponent);
---
+++ app/components/header/header.component.js
@@ -0,0 +1,11 @@
+import template from './header.template.html';
+import './header.css';
+
+class Header {
+
+}
+
+export default {
+    template: template,
+    controller: Header
+};
